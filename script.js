476d1d3c924bafe8b7fd4ca84ae658d90e45a7f6
j-irion <juliusirion@gmx.de>
2022-08-10 13:22:07 +0200

    Extend createBook function
    Added creation of the divs content

--- script.js
+++ script.js
@@ -24,7 +24,29 @@ addBookToLibrary(book1);
 
 function createBook(book) {
   let content = document.querySelector(".content");
+
   let bookDiv = document.createElement("div");
   bookDiv.className = "book";
+
+  let title = document.createElement("span");
+  title.className = "book-title";
+  title.textContent = book.title;
+  bookDiv.appendChild(title);
+
+  let author = document.createElement("span");
+  author.className = "book-author";
+  author.textContent = `by ${book.author}`;
+  bookDiv.appendChild(author);
+
+  let pages = document.createElement("span");
+  pages.className = "book-pages";
+  pages.textContent = `${book.pages} pages`;
+  bookDiv.appendChild(pages);
+
+  let read = document.createElement("span");
+  read.className = "book-read";
+  read.textContent = book.read ? "already read" : "not yet read";
+  bookDiv.appendChild(read);
+
   content.appendChild(bookDiv);
 }
